6019c612379042eb5df8a2a633d5f78dbc3f40fa
Christopher Eric Jude Saldanha <christopher.eric@btech.christuniversity.in>
2025-07-11 00:49:55 +0530

    Update hook.c

--- hook.c
+++ hook.c
@@ -1,40 +1,52 @@
-#include <stdio.h>
-#include <stdarg.h>
-#include <unistd.h>     // for sleep()
-#include <pthread.h>    // for threading
-#include "fishhook.h"
+#import <UIKit/UIKit.h>
+#import "fishhook.h"
 
-// Save the original sscanf (optional)
+// Save original sscanf
 int (*original_sscanf)(const char *str, const char *format, ...);
 
-// Replacement sscanf
+// Hooked version
 int my_sscanf(const char *str, const char *format, ...) {
-    printf("[HOOKED] sscanf() was blocked\n");
-    return 0;  // Always block
+    NSLog(@"[HOOKED] sscanf was blocked");
+    return 0; // block completely
 }
 
-// Thread function to delay the hook
-void *delayed_hook_thread(void *arg) {
-    printf("[HOOK] Waiting 2 minutes before hooking sscanf...\n");
-    sleep(120);  // Wait 120 seconds
+// Run-time hook function
+void activateHook() {
+    NSLog(@"[HOOK] Activating sscanf hook...");
 
     struct rebinding hook;
     hook.name = "sscanf";
     hook.replacement = my_sscanf;
     hook.replaced = (void *)&original_sscanf;
-
     rebind_symbols(&hook, 1);
-    printf("[HOOK] sscanf has now been hooked!\n");
-
-    return NULL;
 }
 
-// Runs automatically when the dylib is loaded
-__attribute__((constructor))
-void init() {
-    printf("[HOOK INIT] Setting up delayed sscanf hook...\n");
+// Create a floating UIButton
+void showFloatingButton() {
+    dispatch_async(dispatch_get_main_queue(), ^{
+        UIWindow *keyWindow = [UIApplication sharedApplication].keyWindow;
 
-    pthread_t thread;
-    pthread_create(&thread, NULL, delayed_hook_thread, NULL);
-    pthread_detach(thread);  // Optional: don’t wait for thread to finish
+        UIButton *hookButton = [UIButton buttonWithType:UIButtonTypeSystem];
+        hookButton.frame = CGRectMake(20, 100, 120, 40);
+        [hookButton setTitle:@"Hook sscanf" forState:UIControlStateNormal];
+        hookButton.backgroundColor = [UIColor colorWithRed:0.1 green:0.6 blue:1 alpha:0.8];
+        [hookButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+        hookButton.layer.cornerRadius = 10;
+        hookButton.layer.borderWidth = 1;
+        hookButton.layer.borderColor = [UIColor whiteColor].CGColor;
+        hookButton.clipsToBounds = YES;
+
+        [hookButton addTarget:[NSBlockOperation blockOperationWithBlock:^{
+            activateHook();
+        }] action:@selector(main) forControlEvents:UIControlEventTouchUpInside];
+
+        [keyWindow addSubview:hookButton];
+    });
+}
+
+// Dylib entry
+__attribute__((constructor))
+static void initializer() {
+    NSLog(@"[INIT] Dylib loaded. Showing hook button...");
+    showFloatingButton();
 }
