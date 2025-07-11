3c6f05981598fcaa63937f7b16f1a37112a036bd
Christopher Eric Jude Saldanha <christopher.eric@btech.christuniversity.in>
2025-07-11 15:39:26 +0530

    Update hook.mm

--- src/hook.mm
+++ src/hook.mm
@@ -10,31 +10,26 @@ typedef int (*sscanf_ptr_t)(const char *, const char *, ...);
 sscanf_ptr_t orig_sscanf = NULL;
 
 bool (*IsInMatchGame)() = (bool(*)())0x102F805C4;
-bool (*IsInLobby)() = (bool(*)())0x1012A09CC;
+bool (*IsInLobby)()     = (bool(*)())0x1012A09CC;
 
-const uint64_t targetPatchAddr = 0x101E3069C;
-uint8_t originalBytes[8] = {0}; // Placeholder
-uint8_t patchBytes[8] = {0x20, 0x00, 0x08, 0xD2, 0xC0, 0x03, 0x5F, 0xD6};
+// Patch 1: Main mod
+const uint64_t patch1Addr = 0x101E3069C;
+uint8_t patch1Bytes[8]    = {0x20, 0x00, 0x08, 0xD2, 0xC0, 0x03, 0x5F, 0xD6};
+uint8_t original1[8]      = {0};
 
-void patchMemory(uint8_t *bytes) {
-    vm_prot_t prot;
-    vm_prot_t prot_max;
+// Patch 2: sscanf override/bypass
+const uint64_t patch2Addr = 0x1012E0B4C;
+uint8_t patch2Bytes[8]    = {0x00, 0x00, 0x80, 0xD2, 0xC0, 0x03, 0x5F, 0xD6};
+uint8_t original2[8]      = {0};
+
+void patchMemory(uint64_t address, uint8_t *bytes) {
+    vm_address_t pageStart = address & ~(vm_page_size - 1);
     mach_port_t task = mach_task_self();
-    vm_address_t pageStart = targetPatchAddr & ~(vm_page_size - 1);
-    vm_region_basic_info_data_64_t info;
-    mach_msg_type_number_t info_count = VM_REGION_BASIC_INFO_COUNT_64;
-    memory_object_name_t object;
-    kern_return_t kr;
 
-    kr = vm_region_64(task, &pageStart, (vm_size_t[]){0}, VM_REGION_BASIC_INFO,
-                      (vm_region_info_t)&info, &info_count, &object);
-
+    kern_return_t kr = vm_protect(task, pageStart, vm_page_size, false, PROT_READ | PROT_WRITE | PROT_EXEC);
     if (kr == KERN_SUCCESS) {
-        kr = vm_protect(task, pageStart, vm_page_size, false, VM_PROT_READ | VM_PROT_WRITE | VM_PROT_COPY | VM_PROT_EXECUTE);
-        if (kr == KERN_SUCCESS) {
-            memcpy((void*)targetPatchAddr, bytes, sizeof(patchBytes));
-            vm_protect(task, pageStart, vm_page_size, false, info.protection);
-        }
+        memcpy((void*)address, bytes, 8);
+        vm_protect(task, pageStart, vm_page_size, false, PROT_READ | PROT_EXEC); // optional restore
     }
 }
 
@@ -52,28 +47,34 @@ void manageHooks() {
     while (true) {
         if (IsInMatchGame() && !hooked) {
             hooked = true;
-            patchMemory(patchBytes);
+
+            patchMemory(patch1Addr, patch1Bytes);
+            patchMemory(patch2Addr, patch2Bytes);
+
             rebind_symbols((struct rebinding[1]) {
                 {"sscanf", (void *)my_sscanf, (void **)&orig_sscanf}
             }, 1);
+
         } else if (IsInLobby() && hooked) {
             hooked = false;
-            patchMemory(originalBytes);  // Restore original bytes
+
+            patchMemory(patch1Addr, original1);
+            patchMemory(patch2Addr, original2);
+
             rebind_symbols((struct rebinding[1]) {
                 {"sscanf", (void *)orig_sscanf, NULL}
             }, 1);
         }
-        usleep(500000); // check every 0.5s
+        usleep(500000); // 0.5s polling
     }
 }
 
 __attribute__((constructor))
 void init() {
-    // Save original memory bytes from patch target
-    memcpy(originalBytes, (void*)targetPatchAddr, sizeof(originalBytes));
+    memcpy(original1, (void*)patch1Addr, sizeof(original1));
+    memcpy(original2, (void*)patch2Addr, sizeof(original2));
 
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), ^{
         manageHooks();
     });
 }
-
